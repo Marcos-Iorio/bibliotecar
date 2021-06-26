4613f6a31774a5b9a017d355c8858538ad9e53b2
Marcos Iorio <marcossiorio@gmail.com>
2021-06-25 21:25:45 -0300

    Agregando filtro por rol

--- php/login.php
+++ php/login.php
@@ -12,29 +12,72 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $pass = $_POST['passL'];
     }
 
-    $stmt = $dbh->prepare('SELECT mail, contraseña from usuarios where mail = "' . $mail .'"');
+    $stmt = $dbh->prepare('SELECT idRol, contraseña from usuarios where mail = "' . $mail .'" LIMIT 1');
     // Ejecutamos
     $stmt->execute();
     // Mostramos los resultados
     $arr = $stmt->fetch(PDO::FETCH_ASSOC);
     
-    if(!empty($arr) && password_verify($pass, $arr['contraseña'])){
-        echo'<script type="text/javascript">
-            alert("Inicio de sesión con éxito");
-            </script>';
-        echo'<script type="text/javascript">
-            setTimeout(window.location.href="http://localhost/Practica%20Profesionalizante/Proyecto%20final/html/inicio.html", 5000);
-            </script>';
+    if($arr['idRol'] === '1'){
+
+        if(!empty($arr) && password_verify($pass, $arr['contraseña'])){
+            echo'<script type="text/javascript">
+                alert("Inicio de sesión con éxito");
+                </script>';
+            echo'<script type="text/javascript">
+                setTimeout(window.location.href="http://localhost/Practica%20Profesionalizante/Proyecto%20final/usuario/inicioUser.php", 5000);
+                </script>';
+        }else{
+            echo'<script type="text/javascript">
+                alert("Credenciales erróneas");
+                </script>';
+            echo'<script type="text/javascript">
+                setTimeout(window.location.href="http://localhost/Practica%20Profesionalizante/Proyecto%20final/html/login.html", 5000);
+                </script>';  
+        }
+    }elseif($arr['idRol'] === '2'){
+
+        if(!empty($arr) && password_verify($pass, $arr['contraseña'])){
+            echo'<script type="text/javascript">
+                alert("Inicio de sesión con éxito");
+                </script>';
+            echo'<script type="text/javascript">
+                setTimeout(window.location.href="http://localhost/Practica%20Profesionalizante/Proyecto%20final/colaborador/inicioColab.php", 5000);
+                </script>';
+        }else{
+            echo'<script type="text/javascript">
+                alert("Credenciales erróneas");
+                </script>';
+            echo'<script type="text/javascript">
+                setTimeout(window.location.href="http://localhost/Practica%20Profesionalizante/Proyecto%20final/html/login.html", 5000);
+                </script>';  
+        }
+    }elseif($arr['idRol'] === '3'){
+
+        if(!empty($arr) && password_verify($pass, $arr['contraseña'])){
+            echo'<script type="text/javascript">
+                alert("Inicio de sesión con éxito");
+                </script>';
+            echo'<script type="text/javascript">
+                setTimeout(window.location.href="http://localhost/Practica%20Profesionalizante/Proyecto%20final/administrador/inicioAdmin.php", 5000);
+                </script>';
+        }else{
+            echo'<script type="text/javascript">
+                alert("Credenciales erróneas");
+                </script>';
+            echo'<script type="text/javascript">
+                setTimeout(window.location.href="http://localhost/Practica%20Profesionalizante/Proyecto%20final/html/login.html", 5000);
+                </script>';  
+        }
     }else{
         echo'<script type="text/javascript">
-            alert("Credenciales erróneas");
-            </script>';
-        echo'<script type="text/javascript">
-            setTimeout(window.location.href="http://localhost/Practica%20Profesionalizante/Proyecto%20final/html/login.html", 5000);
-            </script>';
-            
-        
+                alert("Usuario no registrado!");
+                </script>';
+            echo'<script type="text/javascript">
+                setTimeout(window.location.href="http://localhost/Practica%20Profesionalizante/Proyecto%20final/html/login.html", 5000);
+                </script>';  
     }
+
     
 }else{
     echo "metodo no autorizado";
---
+++ usuario/inicioUser.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://localhost/Practica%20Profesionalizante/Proyecto%20final/css/inicio.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <section id="page">
+        <nav id="sidebar"  onmouseover="toggleSidebar()" onmouseout="toggleSidebar()">
+            <ul id="hovered">
+                <li><img class="logo" src="http://localhost/Practica%20Profesionalizante/Proyecto%20final/assets/Logo sin fondo.png" alt=""><a href=""></a></li>
+                <li id="home"><a href=""><span>Home</span></a></li>
+                <li id="portal-libro"><a href=""><span>Portal de libros</span></a></li>
+            </ul>                   
+        </nav>
+        <main id="main">
+            <h1>Pagina del usuario</h1>
+            <p>
+                Hover en el sidebar para desplegar
+            </p>
+            <button onclick="contacto()" class="buttonInfo tooltip">
+                <i class="fas fa-question"></i>
+                <span class="tooltiptext">¿Tenes dudas? ¡Mandanos un mail!</span>
+            </button>
+        </main>
+      </section>
+</body>
+</html>
+<script src="http://localhost/Practica%20Profesionalizante/Proyecto%20final/js/navbarToggle.js"></script>
