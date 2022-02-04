b02ab9e197b16b599f09f4c40fac455754773f8b
Marcos Iorio <marcossiorio@gmail.com>
2022-02-04 16:47:33 -0300

    estilos en libros

--- php/llenarLibros.php
+++ php/llenarLibros.php
@@ -31,13 +31,11 @@
         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
     </svg>
 </div>															  
-                  <p class="libro-info">';
-                  echo 'Titulo:<span class="portal-info"> ' . $fila['titulo'] . '</span>  <br>';
-                  echo 'Autor:<span class="portal-info"> ' . $fila['nombreAutor']. '</span>  <br>';
-                  echo 'Categoria:<span class="portal-info"> ' . $fila['nombreCategoria'] . '</span>
-                  </p>
-              </div>
-              <div class="etiqueta">
+                  <div class="libro-info">';
+                  echo '<p class="portal-info-autor"> ' . $fila['nombreAutor']. '</p>';
+                  echo '<p class="portal-info-titulo"> ' . $fila['titulo'] . '</p>';
+                  echo '<p class="portal-info-categoria"> ' . $fila['nombreCategoria'] . '</p>
+                  <div class="etiqueta">
                   <p class="etiqueta-info" id="etiqueta-info">';
                       if($fila['stock'] > 0 ){
                           echo "Disponible";
@@ -45,6 +43,8 @@
                           echo "No disponible";
                       } 
              echo ' </div>
+                  </div>
+              </div>
           </a>
       </div>';
   endforeach;
